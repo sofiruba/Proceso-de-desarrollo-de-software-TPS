1afc2b1153c312094faddaa3e20cdf03b49acab9
sofiruba <102734675+sofiruba@users.noreply.github.com>
2026-04-08 10:49:34 -0300

    limpiar

--- src/TP3/ej1/TempAdapter.java
+++ src/TP3/ej1/TempAdapter.java
@@ -13,12 +13,8 @@ class TempAdapter implements SensorCelsius {
     public double leerTemperatura() {
 
         String xml = legacySensor.obtenerTemperaturaXML();
-
-        // 2. Extraer el valor (esto es un parseo simple para el ejemplo)
         String valorStr = xml.replace("<temp>", "").replace("</temp>", "");
         double fahrenheit = Double.parseDouble(valorStr);
-
-        // 3. Convertir de Fahrenheit a Celsius
         double celsius = (fahrenheit - 32) * 5 / 9;
 
         return celsius;
